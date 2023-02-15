25634cb6d1b038979f4bfe3030e842ee34852d8b
Lignito79 <joseangelrenteria1159@gmail.com>
2023-02-15 16:32:38 -0600

    Termine work-experience

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class WorkExperienceComponent {
 
+  workExperience : Array<any> = [];
+
+constructor() { }
+
+ngOnInit(): void {
+
+  let work1 = {
+    fecha: "2018-2022",
+    ubicacion: "Orizaba, Ver",
+    puesto: "CEO",
+    empresa: "Waves",
+    logros : [
+      { descripcion: "Dron autonomo" },
+      { descipcion: "Cluster GPU" },
+    ]
+  };
+
+  let work2 = {
+    fecha: "2015-2018",
+    ubicacion: "Ixtac, Ver",
+    puesto: "CEO",
+    empresa: "Kubeet",
+    logros : [
+      { descripcion: "Cluster hadoop" },
+      { descipcion: "Red Neuronal GAN" },
+    ]
+  };
+
+  this.workExperience.push(work1);
+  this.workExperience.push(work2);
+}
+
 }
